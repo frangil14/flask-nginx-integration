b5f4272da2daca2fd9ee5c128a9659951ea349c0
Francisco Gil <francisconicolasgil@gmail.com>
2024-07-28 20:07:58 -0300

    posgress not run init script

--- web-service/app.py
+++ web-service/app.py
@@ -2,11 +2,12 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import OperationalError
 import time
+import os
 
 app = Flask(__name__)
 
 # Configuración de la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/mydatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'postgresql://username:password@postgres:5432/mydatabase')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -17,7 +18,7 @@ def connect_db_with_retries():
     retries = 5
     while retries > 0:
         try:
-            db.create_all()
+            db.session.execute('SELECT 1')
             break
         except OperationalError:
             retries -= 1
@@ -27,11 +28,16 @@ def connect_db_with_retries():
 
 # Modelo de Usuario
 class User(db.Model):
+    __tablename__ = 'users' 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     age = db.Column(db.Integer, nullable=False)
 
 # Rutas
+@app.route('/')
+def index():
+    return "Flask app is running!"
+
 @app.route('/users', methods=['POST'])
 def create_user():
     data = request.get_json()
