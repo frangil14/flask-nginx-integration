4f366aabbf7029c6e521b6258218a2ec146ba92a
Francisco <francisconicolasgil@gmail.com>
2024-07-28 23:07:05 -0300

    fix: init script working in postgres

--- database/init.sql
+++ database/init.sql
@@ -1,12 +1,39 @@
 -- Crear la base de datos (si no existe)
-CREATE DATABASE IF NOT EXISTS mydatabase;
 
--- Conectarse a la base de datos
-\c mydatabase;
+DO $$
+BEGIN
+    -- Verifica si la base de datos existe
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'mydatabase') THEN
+        -- Crea la base de datos
+        EXECUTE 'CREATE DATABASE mydatabase';
+        
+        -- Mensaje en la consola
+        RAISE NOTICE 'Base de datos creada guachin';
+    ELSE
+        RAISE NOTICE 'La base de datos ya existe guachin';
+    END IF;
+END
+$$;
 
--- Crear la tabla de usuarios (si no existe)
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    age INTEGER NOT NULL
-);
+-- Conecta a la base de datos
+\c mydatabase
+
+DO $$
+BEGIN
+    -- Verifica si la tabla "users" existe
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
+        -- Crea la tabla "users"
+        EXECUTE '
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            age INTEGER NOT NULL
+        )';
+        
+        -- Mensaje en la consola
+        RAISE NOTICE 'Tabla users creada guachin';
+    ELSE
+        RAISE NOTICE 'La tabla users ya existe guachin';
+    END IF;
+END
+$$;
